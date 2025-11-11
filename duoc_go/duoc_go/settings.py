8877d6af1f779f80e423e5d464423500f3a6a4cd
Sebastian Michell <base@MacBook-Pro-de-Sebastian.local>
2025-11-10 22:04:16 -0300

    Cambiar configuración de email a Puerto 587 (TLS)

--- duoc_go/duoc_go/settings.py
+++ duoc_go/duoc_go/settings.py
@@ -122,9 +122,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # --- 10. Configuración de Email (SendGrid) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net' 
-EMAIL_PORT = 465             
-EMAIL_USE_TLS = False        
-EMAIL_USE_SSL = True         
+EMAIL_PORT = 587             
+EMAIL_USE_TLS = True        
+EMAIL_USE_SSL = False         
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'jexfryxd@gmail.com'
