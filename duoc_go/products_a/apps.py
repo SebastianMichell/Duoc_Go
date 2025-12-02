f57748a7012fb3f3eb082cadd548fb00e9094414
SebastianMichell <129518712+SebastianMichell@users.noreply.github.com>
2025-12-02 08:47:20 -0300

    Delete duoc_go directory

--- duoc_go/products_a/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductsAConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products_a'
--- duoc_go/products_b/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Más adelante agregaremos rutas reales si quieres una vista para productos
-    # Por ahora basta con esto para que no dé error
-]
